fd06e65d5fafffcdc226cc36fc93381d6ed34b63
Bao Quan <lethanhbaoquan2005@gmail.com>
2025-11-05 17:15:34 +0700

    khởi tạo service payment cấu hình port cho service

--- filter-service/src/main/java/com/kodlamaio/filterservice/FilterServiceApplication.java
+++ filter-service/src/main/java/com/kodlamaio/filterservice/FilterServiceApplication.java
@@ -1,13 +1,16 @@
 package com.kodlamaio.filterservice;
 
+import com.kodlamaio.common.constants.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableCaching
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {Paths.ExceptionHandlerBasePackage, Paths.Filter.ServiceBasePackage})
 public class FilterServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FilterServiceApplication.class, args);
     }
-
 }
