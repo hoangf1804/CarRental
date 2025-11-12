0d2bd050b5bf3ede5b6d021cd23cedd5a70bfb1a
Giang-boop <truonggiang02032005@gmail.com>
2025-11-13 01:20:24 +0700

    Delete src directory

--- src/main/java/com/kodlamaio/rentalservice/repository/RentalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kodlamaio.rentalservice.repository;
-
-import com.kodlamaio.rentalservice.entities.Rental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentalRepository extends JpaRepository<Rental, String> {
-}
