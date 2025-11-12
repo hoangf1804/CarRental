83a4613754a0ee666b347a07ab78c168f369d6d9
Giang-boop <truonggiang02032005@gmail.com>
2025-11-13 01:14:59 +0700

    Add entities and repository to rental-service

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:17-jdk-slim
+FROM eclipse-temurin:17-jdk
 WORKDIR /app
 COPY target/rental-service-0.0.1-SNAPSHOT.jar /app/rental-service.jar
 ENTRYPOINT ["java", "-jar", "/app/rental-service.jar"]
---
+++ src/main/java/com/kodlamaio/rentalservice/business/abstracts/RentalService.java
@@ -0,0 +1,19 @@
+package com.kodlamaio.rentalservice.business.abstracts;
+
+import com.kodlamaio.common.dto.CustomerRequest;
+import com.kodlamaio.rentalservice.business.dto.requests.create.CreateRentalRequest;
+import com.kodlamaio.rentalservice.business.dto.requests.update.UpdateRentalRequest;
+import com.kodlamaio.rentalservice.business.dto.responses.create.CreateRentalResponse;
+import com.kodlamaio.rentalservice.business.dto.responses.get.GetAllRentalsResponse;
+import com.kodlamaio.rentalservice.business.dto.responses.get.GetRentalResponse;
+import com.kodlamaio.rentalservice.business.dto.responses.update.UpdateRentalResponse;
+
+import java.util.List;
+
+public interface RentalService {
+    List<GetAllRentalsResponse> getAll();
+    GetRentalResponse getById(String id);
+    CreateRentalResponse add(CreateRentalRequest request, CustomerRequest customerRequest);
+    UpdateRentalResponse update(UpdateRentalRequest request, String id, CustomerRequest customerRequest);
+    void delete(String id);
+}
