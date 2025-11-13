4f25f4f86a1f88ad19c3cea7f962fa8332f060fa
Giang-boop <truonggiang02032005@gmail.com>
2025-11-13 09:52:09 +0700

    feat(rental-service): add api, business, configuration, and kafka modules

---
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/api/controllers/RentalsController.java
@@ -0,0 +1,65 @@
+package com.kodlamaio.rentalservice.api.controllers;
+
+import com.kodlamaio.common.constants.Paths;
+import com.kodlamaio.common.constants.Roles;
+import com.kodlamaio.common.dto.CustomerRequest;
+import com.kodlamaio.common.utils.jwt.customer.ParseJwtToCustomerRequest;
+import com.kodlamaio.rentalservice.business.abstracts.RentalService;
+import com.kodlamaio.rentalservice.business.dto.requests.create.CreateRentalRequest;
+import com.kodlamaio.rentalservice.business.dto.requests.update.UpdateRentalRequest;
+import com.kodlamaio.rentalservice.business.dto.responses.create.CreateRentalResponse;
+import com.kodlamaio.rentalservice.business.dto.responses.get.GetAllRentalsResponse;
+import com.kodlamaio.rentalservice.business.dto.responses.get.GetRentalResponse;
+import com.kodlamaio.rentalservice.business.dto.responses.update.UpdateRentalResponse;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping(Paths.Rental.Prefix)
+public class RentalsController {
+    private final RentalService service;
+
+    public RentalsController(RentalService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    @PreAuthorize(Roles.AdminOrDeveloperOrModerator)
+    public List<GetAllRentalsResponse> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping(Paths.IdSuffix)
+    @PostAuthorize(Roles.AdminOrDeveloperOrModerator + "|| returnObject.customerId == #jwt.subject")
+    public GetRentalResponse getById(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return service.getById(id);
+    }
+
+    @PostMapping
+    public CreateRentalResponse add(@Valid @RequestBody CreateRentalRequest request, @AuthenticationPrincipal Jwt jwt) {
+        CustomerRequest customerRequest = ParseJwtToCustomerRequest.getCustomerInformation(jwt);
+
+        return service.add(request, customerRequest);
+    }
+
+    @PutMapping(Paths.IdSuffix)
+    @PreAuthorize(Roles.AdminOrDeveloper)
+    public UpdateRentalResponse update(@Valid @RequestBody UpdateRentalRequest request, @PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        CustomerRequest customerRequest = ParseJwtToCustomerRequest.getCustomerInformation(jwt);
+
+        return service.update(request, id, customerRequest);
+    }
+
+    @DeleteMapping(Paths.IdSuffix)
+    @PreAuthorize(Roles.AdminOrDeveloper)
+    public void delete(@PathVariable String id) {
+        service.delete(id);
+    }
+}
---
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/business/dto/responses/get/GetAllRentalsResponse.java
@@ -0,0 +1,26 @@
+package com.kodlamaio.rentalservice.business.dto.responses.get;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetAllRentalsResponse {
+    private String id;
+    private String carId;
+    private String customerId;
+    private LocalDateTime dateStarted;
+    private int rentedForDays;
+    private double dailyPrice;
+    private double totalPrice;
+    private String customerUserName;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerEmail;
+}
