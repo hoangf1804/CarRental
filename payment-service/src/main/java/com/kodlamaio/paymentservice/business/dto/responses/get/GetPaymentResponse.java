ee016272453ae5986faa551f3b2cab0d5d498b8b
XuanHoang <leexuaanhoangf@gmail.com>
2025-11-27 23:37:32 +0700

    viết các api cho payment service

---
+++ payment-service/src/main/java/com/kodlamaio/paymentservice/business/dto/responses/get/GetPaymentResponse.java
@@ -0,0 +1,25 @@
+package com.kodlamaio.paymentservice.business.dto.responses.get;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetPaymentResponse {
+    private String id;
+    private String customerId;
+    private String cardNumber;
+    private String cardholder;
+    private int cardExpirationYear;
+    private int cardExpirationMonth;
+    private String cardCvv;
+    private double balance;
+    private String customerUserName;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerEmail;
+}
